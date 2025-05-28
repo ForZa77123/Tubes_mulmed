78750a38ac35491a0b4e4283ad502b57ffbbe2e6
Felixx <felixpasaribu01@gmail.com>
2025-05-28 16:21:21 +0700

    open camera

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-import mediapipe
-print(mediapipe.__version__)
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+    print("Error: Could not open video.")
+    exit()
+    
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame.")
+        break
+    
+    cv2.imshow('Video Feed', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
